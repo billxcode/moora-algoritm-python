1b03e7a862d6e91825e1bc316d205f4582e47995
billxcode <xcodebill@gmail.com>
2018-10-04 07:02:14 +0700

    showing len column and row

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 #!/bin/bash
+LENGTH_OF_ROW = 0
+LENGTH_OF_COLUMN = 0
 
 with open('sample.txt', 'r') as file:
     for data in file:
@@ -8,3 +10,9 @@ with open('sample.txt', 'r') as file:
                 if int(info)%2!=0:
                     genap+=1
         print(data+" => "+str(genap))
+        LENGTH_OF_COLUMN = len(info)
+    LENGTH_OF_ROW = len(data)
+
+    print("PANJANG KOLOM = "+str(LENGTH_OF_COLUMN))
+    print("PANJANG BARIS = "+str(LENGTH_OF_ROW))
+        
