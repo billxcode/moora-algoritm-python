2f2bddd2dd6f66f698bdc39eb1c64b07ddbbe16d
billxcode <xcodebill@gmail.com>
2018-10-05 19:13:03 +0700

    add quadrat code to make all element squared

--- index.py
+++ index.py
@@ -3,16 +3,21 @@ LENGTH_OF_ROW = 0
 LENGTH_OF_COLUMN = 0
 
 with open('sample.txt', 'r') as file:
+    with open('new_sample.txt', 'w') as newfile:
+        for newdata in file:
+            data_info = newdata.split(' ')
+            data_count = len(data_info)
+            for index, newdimension in enumerate(data_info):
+                if newdimension!='' and newdimension!='\n':
+                    if index<(data_count-2):
+                        newfile.write(newdimension.strip()+',')
+                    else:
+                        newfile.write(newdimension.strip()+'\n')
+with open('new_sample.txt', 'r') as file:
     for data in file:
-        genap=0
-        for info in data.split(" "):
-            if info.strip()!='':
-                if int(info)%2!=0:
-                    genap+=1
-        print(data+" => "+str(genap))
-        LENGTH_OF_COLUMN = len(info)
-    LENGTH_OF_ROW = len(data)
+       datainfo = data.split(',')
+       for index, newdata in enumerate(datainfo):
+           datainfo[index] = int(datainfo[index])**2
+           print(str(datainfo[index])+' ', end='')
 
-    print("PANJANG KOLOM = "+str(LENGTH_OF_COLUMN))
-    print("PANJANG BARIS = "+str(LENGTH_OF_ROW))
         
