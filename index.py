8da8c05119bd5de12d9bfebd5cb89dd589a51841
Bill Tanthowi jauhari <bill.tanthowi.j@gmail.com>
2018-10-06 16:41:36 +0700

    add method to sum all rows

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ LENGTH_OF_ROW = 0
 LENGTH_OF_COLUMN = 0
 
 with open('data/sample.txt', 'r') as file:
-    with open('result/sqr.txt', 'w') as newfile:
+    with open('result/clean.txt', 'w') as newfile:
         for newdata in file:
             data_info = newdata.split(' ')
             data_count = len(data_info)
@@ -14,10 +14,10 @@ with open('data/sample.txt', 'r') as file:
                     else:
                         newfile.write(newdimension.strip()+'\n')
 
-print('success sqr dimension!')
+print('success clean dimension!')
 
-with open('result/sqr.txt', 'r') as file:
-    with open('result/sumrow.txt', 'w') as newfile:
+with open('result/clean.txt', 'r') as file:
+    with open('result/sqr.txt', 'w') as newfile:
         for data in file:
             datainfo = data.split(',')
             datacount = len(datainfo)
@@ -28,5 +28,21 @@ with open('result/sqr.txt', 'r') as file:
                 else:
                     newfile.write('\n')
 
-print('success sum all row!')
+print('success sqr dimension')
+
+with open('result/sqr.txt') as file:
+    with open('result/sumrow.txt', 'w') as newfile:
+        for data in file:
+            datainfo = data.split(' ')
+            datacount = len(datainfo) 
+            sum_row = 0
+            for index, newdata in enumerate(datainfo):
+                if newdata!='\n':
+                    sum_row += int(newdata)
+            # print('=> '+str(sum_row))
+            newfile.write(str(sum_row).rstrip('\n')+',')
+
+print('success sum all rows')
+
+
 # with open('total_perbaris.txt', 'r')
